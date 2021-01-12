86e66937d535ced192b1d369132d1976eaa104fd
TheOmid <omid.shahidi.2012@gmail.com>
2021-01-12 07:05:56 -0500

    styling changes

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const ListLink = props => (
 )
 
 const Header = ({ siteTitle }) => (
-  <header class="my-0 mx-auto max-w-5xl py-6 px-4 text-center md:text-justify">
+  <header class="my-0 mx-auto max-w-5xl py-4 px-4 text-center md:text-justify">
         <Link to="/">
           <span class="text-gold font-bold text-2xl">{siteTitle}</span>
         </Link>
