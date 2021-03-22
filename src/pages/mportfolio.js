070f056d1102746e393451269b580aa48b8520b5
TheOmid <omid.shahidi.2012@gmail.com>
2021-03-21 20:51:39 -0400

    Added portfolio page for honors

---
+++ src/pages/mportfolio.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const MPortfolio = () => (
+  <Layout>
+    <SEO title="MPortfolio" />
+    <h1 class="text-primary text-4xl md:text-6xl text-center px-2 mt-10 mb-10 tracking-tight">MPortfolio.</h1>
+
+    <h2 class="text-primary text-2xl md:text-4xl text-center px-2 mt-10 mb-10 tracking-tight">This page is for Honors Portfolio</h2>
+  </Layout>
+)
+
+export default MPortfolio
