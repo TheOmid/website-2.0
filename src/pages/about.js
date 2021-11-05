f5b08392216aa7a9e9edde8aaa35cad5606cca6d
TheOmid <omid.shahidi.2012@gmail.com>
2021-11-04 20:45:54 -0400

    updates for fall 2021

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,25 +17,19 @@ const About = () => (
         <Image alt="Profile Pic" filename="profile.jpg" />
       </div>
       <p class="float-right md:pl-4 max-w-screen-sm my-1 md:mx-2 text-justify md:tracking-tight tracking-tighter">
-        Hello, I'm Omid Shahidi. I am a sophomore studying Computer Science and
+        Hello, I'm Omid Shahidi. I am a junior studying Computer Science and
         Data Science (with a minor in Philosophy) at the University of
         Michigan-College of Engineering. As an aspiring engineer, I love working
         with multidisciplinary teams, and I believe my biggest strength lies in
         collaboration with engineers, artists, and designers. Right now, my
         research team and I are aiming to turn sleep data into ambient,
         electronic music! Outside of school, I am always trying to learn new
-        things, and to explore different hobbies. Right now, I am learning more
-        about ML, Algorithms, and APIs (
-        <a
-          class="underline"
-          rel="noreferrer"
-          target="_blank"
-          href="https://github.com/TheOmid"
-        >
-          github
-        </a>
-        ). I am watching countless hours on YouTube about Sound Design, Music
-        Production, and Audio Engineering. I host my podcast,{" "}
+        things, and to explore different hobbies. Right now, I am very interested
+        in Operating Systems, Distributed Systems, and Cloud Computing.  
+
+        I am watching countless hours of Ted Talks on YouTube.
+        
+        Once upon a time, I hosted a podcast,{" "}
         <a
           class="italic underline"
           rel="noreferrer"
@@ -44,9 +38,11 @@ const About = () => (
         >
           Thoughts about Thoughts
         </a>
-        . Check it out! Each month, I have a conversation with my awesome guests
+        ! Check it out! It was about having conversations with my awesome guests
         about their life stories revolving around a topic related to personal
-        growth and philosphy. I enjoy reading about philosphy, and I write about
+        growth and philosphy. I am working on something new, so stay tuned!👀 
+        
+        I enjoy reading about philosphy, and I write about
         it in my{" "}
         <Link class="underline" to="/blog/">
           blog
@@ -61,8 +57,10 @@ const About = () => (
         >
           Explorers Grand Slam
         </a>{" "}
-        Challenge. My life goal is to become a teacher or mentor for someone. If
-        you like to chat use the{" "}
+        Challenge. My life goal is to become a teacher or mentor for someone. I feel like everyday
+        I am getting closer to this goal. 
+        
+        If you like to chat use the{" "}
         <Link class="underline" to="/contact/">
           contact page
         </Link>
@@ -83,20 +81,10 @@ const About = () => (
     <Activity symbol="💻" text="Working on personal" to="/projects/">
       projects
     </Activity>
-    <Activity
-      symbol="🌌"
-      text="Hosting Thoughts about Thoughts Podcast"
-      to=""
-    ></Activity>
+    <Activity symbol="🌌" text="IAing for" to="https://eecs280staff.github.io/eecs280.org">eecs 280</Activity>
     <Activity symbol="🎧" text="Jamming to" special="music"></Activity>
-    <Activity
-      symbol="📗"
-      text="Reading the Meditations by Marcus Aurelius"
-    ></Activity>
-    <Activity
-      symbol="🎹"
-      text="Learning to make Electronic music and DJ"
-    ></Activity>
+    <Activity symbol="📗" text="I need a good book right now..."></Activity>
+    <Activity symbol="🎹" text="Learning to DJ and make electronic music"></Activity>
   </Layout>
 )
 
